b0050a5bd6ccc50ce5d4771f7bf6ad0040ff3579
yh <dygks4308@gmail.com>
2023-10-13 17:57:01 +0900

    algorithm

---
+++ algorithmStudy/src/backjoon/algorithm_9093.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class algorithm_9093 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = br.read();
+		char[] ch = new char[N];
+
+
+	}
+}
