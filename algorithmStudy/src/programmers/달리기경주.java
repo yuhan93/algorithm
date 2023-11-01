a0cd3181aa22a74a58bc032200adbd4abf9a01fd
yh <dygks4308@gmail.com>
2023-11-01 19:35:32 +0900

    algorithm

--- algorithmStudy/src/programmers/달리기경주.java
+++ algorithmStudy/src/programmers/달리기경주.java
@@ -1,8 +1,19 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class 달리기경주 {
 	public static void main(String[] args) {
 
-		
+		String a = "hello";
+		int b = 3;
+		String c = "";
+		String sb = "";
+		for(int i = 0; i < a.length(); i ++){
+			for(int j = 0; j < b; j++){
+				sb += (a.charAt(i));
+			}
+		}
+		System.out.println(sb);
 	}
 }
