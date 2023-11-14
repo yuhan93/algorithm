1fe22c78591ce31fd5a29b16dc55d087eea0d5bd
yh <dygks4308@gmail.com>
2023-11-14 20:01:09 +0900

    algorithm 20231114

---
+++ algorithmStudy/src/programmers/L0d캐릭터의좌표.java
@@ -0,0 +1,31 @@
+package programmers;
+
+public class L0d캐릭터의좌표 {
+	public static void main(String[] args) {
+		String[] keyinput = {"left", "right", "up", "right", "right"};
+		int[] board = {11,11};
+		System.out.println("@@@" + board[0] / 2);
+		System.out.println("@@@" + board[1] / 2);
+		int x = board[0] / 2;
+		int y = board[1] / 2;
+		int a = 0;
+		int b = 0;
+		for(int i = 0 ; i < keyinput.length; i++){
+			if(keyinput[i].equals("left")){
+
+			}
+			if(keyinput[i].equals("right")){
+
+			}
+			if(keyinput[i].equals("up")){
+
+			}
+			if(keyinput[i].equals("down")){
+
+			}
+		}
+
+
+
+	}
+}
