4108275ebd244b161cac7fac5cd9d096b12203dd
yh <dygks4308@gmail.com>
2023-11-15 19:46:25 +0900

    algorithm 20231115

---
+++ algorithmStudy/src/programmers/L0치킨쿠폰.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class L0치킨쿠폰 {
+	public static void main(String[] args) {
+		// 치킨 시켜먹음
+		// 한마리당 쿠폰 하나 줌
+		// 쿠폰 10개 모으면 치킨 1마리 주는데
+		// 거기에도 쿠폰 들어가있음
+		int chicken = 1081;
+		int answer = 0;
+
+		if(chicken / 10 == 10){
+			answer = answer +1;
+		}
+
+	}
+}
